68033f9e799ca07ea770bea89033044ba64f5e28
JoaoVitor-developer <joaovitorleonardodias@gmail.com>
2025-03-26 16:44:24 -0300

    atualizaçao

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ function nextImage() {
     }
     document.getElementById('radio' + count).checked = true;
 }
+
+var menuItem = document.querySelectorAll('.item-menu')
+
+function selectLink() {
+    menuItem.forEach((item) => 
+        item.classList.remove('ativo'))
+    this.classList.add('ativo')
+}
+
+menuItem.forEach((item) => 
+    item.addEventListener('click', selectLink)
+)
