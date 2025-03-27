0afc7d11740429852c0c33ed6b4e98c3156c53a9
JoaoVitor-developer <joaovitorleonardodias@gmail.com>
2025-03-27 16:05:01 -0300

    atualizaçao

--- script.js
+++ script.js
@@ -24,3 +24,10 @@ function selectLink() {
 menuItem.forEach((item) => 
     item.addEventListener('click', selectLink)
 )
+
+var btnExp = document.querySelector('#btn-exp')
+var menuSide = document.querySelector('.menu-lateral')
+
+btnExp.addEventListener('click', function(){
+    menuSide.classList.toggle('expadir')
+})
