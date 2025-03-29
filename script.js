0d010b7b4e4ee108bbec94ad71ecd81c195741d8
JoaoVitor-developer <joaovitorleonardodias@gmail.com>
2025-03-29 03:07:59 -0300

    atualizaçao

--- script.js
+++ script.js
@@ -27,11 +27,20 @@ menuItem.forEach((item) =>
     item.addEventListener('click', selectLink)
 )
 
-//------------------------------------------------------------------
+// -----------------------------------------------------------------
 
 var btnExp = document.querySelector('#btn-exp')
 var menuSide = document.querySelector('.menu-lateral')
+var ppaImg = document.getElementById('ppa-imagem')
 
 btnExp.addEventListener('click', function(){
     menuSide.classList.toggle('expadir')
+
+    if (ppaImg.style.visibility === 'hidden') {
+        ppaImg.style.visibility = 'visible'
+    } else {
+        ppaImg.style.visibility = 'hidden'
+    }
 });
+
+// -----------------------------------------------------------------
